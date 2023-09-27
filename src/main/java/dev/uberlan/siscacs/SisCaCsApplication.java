6184454a6e6fc456dfc87e23772c9d9c02e86e80
Uberlan Nunes <uberlannunes@gmail.com>
2023-09-27 17:42:42 -0300

    setup inicial

---
+++ src/main/java/dev/uberlan/siscacs/SisCaCsApplication.java
@@ -0,0 +1,13 @@
+package dev.uberlan.siscacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisCaCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisCaCsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uberlan/siscacs/SisCaCsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uberlan.siscacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisCaCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
