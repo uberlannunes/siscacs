33eeb110eb7b4381eb049d6fce3eb3e30c70122d
Uberlan Nunes <uberlannunes@gmail.com>
2023-10-04 18:25:28 -0300

    adicionando entity Municao

---
+++ src/main/java/dev/uberlan/siscacs/domain/Municao.java
@@ -0,0 +1,61 @@
+package dev.uberlan.siscacs.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "municoes")
+class Municao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "arma_id", nullable = false)
+    private Arma arma;
+
+    @Column(nullable = false)
+    @PositiveOrZero
+    private int quantidade;
+
+    public Municao() {
+    }
+
+    public Municao(Arma arma, int quantidade) {
+        this.arma = arma;
+        this.quantidade = quantidade;
+    }
+
+    public Municao(UUID id, Arma arma, int quantidade) {
+        this.id = id;
+        this.arma = arma;
+        this.quantidade = quantidade;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Arma getArma() {
+        return arma;
+    }
+
+    public void setArma(Arma arma) {
+        this.arma = arma;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
