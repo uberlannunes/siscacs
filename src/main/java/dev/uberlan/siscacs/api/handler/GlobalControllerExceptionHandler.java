924e93c09b364cc00c4ca6cea56b08b45faa8951
Uberlan Nunes <uberlannunes@gmail.com>
2023-11-20 18:58:17 -0300

    adicionando o tratamento de erros global da aplicação

---
+++ src/main/java/dev/uberlan/siscacs/api/handler/GlobalControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package dev.uberlan.siscacs.api.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("tituloPagina", "Não foi possível processar a solicitação.");
+        mav.addObject("mensagemErro", e.getMessage());
+
+        mav.setViewName("/erro");
+        return mav;
+    }
+}
